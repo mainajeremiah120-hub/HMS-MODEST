e2744520a47be156ff7959c9ef96c4750848533b
jeremy <mainajeremiah120@gmail.com>
2026-05-21 16:26:25 +0300

    Fixed sidebar

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -4,15 +4,15 @@ import { Outlet } from "react-router-dom";
 
 function Layout() {
   return (
-    <div className="flex min-h-screen">
+    <div className="flex h-screen overflow-hidden">
       <div className="no-print">
         <Sidebar />
       </div>
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col overflow-hidden">
         <div className="no-print">
           <Navbar />
         </div>
-        <main className="p-6 bg-gray-100 flex-1">
+        <main className="p-6 bg-gray-100 flex-1 overflow-y-auto">
           <Outlet />
         </main>
       </div>
